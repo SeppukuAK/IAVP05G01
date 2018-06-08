bd28a89ecde482804946e36a4773ed194b64d195
Adriana02 <adridelc@ucm.es>
2018-06-08 22:46:55 +0200

    los fantasmitas no se salen de los limites

--- Assets/IAVP5G01/Scripts/Ghosts/Flock/Flock.cs
+++ Assets/IAVP5G01/Scripts/Ghosts/Flock/Flock.cs
@@ -1,119 +1,126 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class Flock : MonoBehaviour
 {
     /// <summary>
     /// Velocidad a la que se mueve el fantasma
     /// </summary>
-    public float SpeedIndividual;
-    public float SpeedGroup;
+    [SerializeField] private float speedIndividual;
+    [SerializeField] private float speedGroup;
 
     /// <summary>
     /// Velocidad a la que rota el fantasma
     /// 4.0f
     /// </summary>
-    public float RotationSpeed;
+    [SerializeField] private float rotationSpeed;
 
     /// <summary>
     /// Distancia a otros fantasmas que necesita para poder congregarse
     /// 3.0f
     /// </summary>
-    public float NeighbourDistance;
+    [SerializeField] private float neighbourDistance;
 
-    ///// <summary>
-    ///// Direccion promedio
-    ///// </summary>
-    //Vector3 averageHeading;
+    /// <summary>
+    /// Tiempo que tarda el agente en saber si los fantasmas están agrupados o no. Cuanto menor sea, mejor
+    /// </summary>
+    [SerializeField] private float dumbTime;
 
-    ///// <summary>
-    ///// Posición promedio
-    ///// </summary>
-    //Vector3 averagePosition;
-
+    /// <summary>
+    /// Velocidad real del fantasma. Si está en grupo tiene el valor de speedGroup y si está solo tiene el de speedIndividual
+    /// </summary>
     private float speed;
+
+    /// <summary>
+    /// tamaño del grupo de fantasmas
+    /// </summary>
     private int groupSize;
 
     void Start()
     {
-        speed = SpeedIndividual;
+        speed = speedIndividual;
         groupSize = 0;
+        StartCoroutine(ApplyRules());
+        
     }
 
     void Update()
     {
-        if (Random.Range(0, 5) < 1)
-            ApplyRules();
-
-        if (groupSize == 0)
-        {
-            speed = SpeedIndividual;
-
-            //Obtenemos la dirección del grupo y giramos al pez gradualmente
-            Vector3 direction = GlobalFlock.GoalPos;
-
-            //Si la dirección es distinta a la actual, rotamos gradualmente
-            if (direction != Vector3.zero)
-                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), RotationSpeed * Time.deltaTime);
-        }
-
-        //Mueve el pez hacia delante
+        //Mueve el fantasma hacia delante
         transform.Translate(0, 0, Time.deltaTime * speed);
     }
 
-    /// <summary>
-    /// Hace todo el trabajo de congregarse
-    /// </summary>
-    private void ApplyRules()
+    private IEnumerator ApplyRules()
     {
-        //Cada pez necesita información de todos los demás fantasmas
-        GameObject[] allGhosts = GlobalFlock.AllGhosts;
-
-        Vector3 center = Vector3.zero;  //Centro del grupo
-        Vector3 avoid = Vector3.zero;   //Vector para evitar colisionar a otros fantasmas
-
-        Vector3 goalPos = GlobalFlock.GoalPos; //Posición a la que se dirigen
-
-        float distance; //Aux
-
-        groupSize = 0; //Cuantos fantasmas están juntos a este
-
-        foreach (GameObject ghost in allGhosts)
+        while (true)
         {
-            //Si el fantasma no es él mismo
-            if (ghost != this.gameObject)
+            //Cada pez necesita información de todos los demás fantasmas
+            GameObject[] allGhosts = GlobalFlock.AllGhosts;
+
+            Vector3 center = Vector3.zero;  //Centro del grupo
+            Vector3 avoid = Vector3.zero;   //Vector para evitar colisionar a otros fantasmas
+
+            Vector3 goalPos = GlobalFlock.GoalPos; //Posición a la que se dirigen
+
+            float distance; //Aux
+
+            groupSize = 0; //Cuantos fantasmas están juntos a este
+
+            foreach (GameObject ghost in allGhosts)
             {
-                //Calculamos la distancia a él.
-                distance = Vector3.Distance(ghost.transform.position, transform.position);
-
-                //Si forma parte de su grupo
-                if (distance <= NeighbourDistance)
+                //Si el fantasma no es él mismo
+                if (ghost != this.gameObject)
                 {
-                    //Calculamos el centro
-                    center += ghost.transform.position;
+                    //Calculamos la distancia a él.
+                    distance = Vector3.Distance(ghost.transform.position, transform.position);
 
-                    //Si estamos muy cerca de él, lo tenemos que evitar
-                    if (distance < 1.0f)
-                        avoid += (transform.position - ghost.transform.position);
+                    //Si forma parte de su grupo
+                    if (distance <= neighbourDistance)
+                    {
+                        //Calculamos el centro
+                        center += ghost.transform.position;
 
-                    //Aumentamos el número de fantasmas en el grupo
-                    groupSize++;
+                        //Si estamos muy cerca de él, lo tenemos que evitar
+                        if (distance < 2.0f)
+                            avoid += (transform.position - ghost.transform.position);
+
+                        //Aumentamos el número de fantasmas en el grupo
+                        groupSize++;
+                    }
                 }
             }
-        }
 
-        //Si el fantasma está en un grupo
-        if (groupSize > 0)
-        {
-            //Obtenemos el centro total del grupo y la velocidad total del grupo
-            center = center / groupSize + (goalPos - transform.position);
-            speed = SpeedGroup;
+            Vector3 direction;
 
-            //Obtenemos la dirección del grupo y giramos al pez gradualmente
-            Vector3 direction = (center + avoid) - transform.position;
+            if (groupSize == 0)
+            {
+                speed = speedIndividual;
+
+                //Obtenemos la dirección del grupo y giramos al fantasma gradualmente
+                direction = GlobalFlock.GoalPos - transform.position;
+
+               
+            }
+
+            //Si el fantasma está en un grupo
+            else
+            {
+                //Obtenemos el centro total del grupo y la velocidad total del grupo
+                center = center / groupSize + (goalPos - transform.position);
+                speed = speedGroup;
+
+                //Obtenemos la dirección del grupo y giramos alfantasma gradualmente
+                 direction = (center + avoid) - transform.position;
+
+            }
 
             //Si la dirección es distinta a la actual, rotamos gradualmente
             if (direction != Vector3.zero)
-                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), RotationSpeed * Time.deltaTime);
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), rotationSpeed * Time.deltaTime);
+
+            yield return new WaitForSeconds(dumbTime);
+
         }
     }
 }
+
--- Assets/IAVP5G01/Scripts/Ghosts/Flock/GlobalFlock.cs
+++ Assets/IAVP5G01/Scripts/Ghosts/Flock/GlobalFlock.cs
@@ -1,53 +1,56 @@
 ﻿using UnityEngine;
-
+using System.Collections;
 public class GlobalFlock : MonoBehaviour
 {
     //Dimensiones del escenario
-
-    //TODO: TOCAR ESTA MIERDA
     [SerializeField] private int width;
     [SerializeField] private int depth;
     [SerializeField] private int height;
-    //[SerializeField] private int sceneTopY = 15;
-    //[SerializeField] private int sceneBotY = -3;
-    [SerializeField] private Vector3 offset;
-
+    [SerializeField] private int offsetY;
+    [SerializeField] private float seconds;
     /// <summary>
-    /// Prefab del Fantasma para crealos en el grupo
+    /// Prefab del Fantasma para crearlos en el grupo
     /// </summary>
     public GameObject GhostPrefab;
 
     /// <summary>
-    /// Número de peces que crea el Manager
+    /// Número de fantasmas que crea el Manager
     /// </summary>
-    private static int numFish = 20;
+    private static int numGhost = 20;
 
     /// <summary>
     /// Array con todos los Fantasmas
     /// </summary>
-    public static GameObject[] AllGhosts = new GameObject[numFish];
+    public static GameObject[] AllGhosts = new GameObject[numGhost];
 
     /// <summary>
-    /// Tiene que estar en el centro de los fantasmas
+    /// Punto de destino de los fantasmas. Tiene que estar en el centro de los fantasmas
     /// </summary>
     public static Vector3 GoalPos = Vector3.zero;
 
     // Use this for initialization
     void Start()
     {
-        for (int i = 0; i < numFish; i++)
+        for (int i = 0; i < numGhost; i++)
         {
-            Vector3 pos = offset + new Vector3(Random.Range(-width, width), Random.Range(-height, height), Random.Range(-depth, depth));
+            Vector3 pos = new Vector3(Random.Range(-width, width), Random.Range(-height, height) + offsetY, Random.Range(-depth, depth));
             AllGhosts[i] = Instantiate(GhostPrefab, pos, Quaternion.identity);
         }
+        StartCoroutine(ModifyGoal());
+
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Corrutina que modifica el punto de destino del fantasma cada x segundos
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator ModifyGoal()
     {
-        if (Random.Range(0,200) < 1)
+        while (true)
         {
-            GoalPos = offset + new Vector3(Random.Range(-width, width), Random.Range(-height, height), Random.Range(-depth, depth));
+            GoalPos = new Vector3(Random.Range(-width, width), Random.Range(-height, height) + offsetY, Random.Range(-depth, depth));
+            yield return new WaitForSeconds(seconds);
         }
+        
     }
 }
